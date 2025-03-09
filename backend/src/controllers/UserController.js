64632ec29fcfe351e127907336f6d3db5b1a8531
RumiAxolotl <rumiaxolotl@gmail.com>
2025-03-09 11:29:46 +0700

    initialize frontend and backend structure with basic configurations and user authentication routes

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("Signup route");
+}
+
+export const signin = (req, res) => {
+    res.send("Signin route");
+}
+
+export const signout = (req, res) => {
+    res.send("Signout route");
+}
---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    }, username: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 6,
+    },
+
+    avatar: {
+        type: String,
+        default: "",
+    },
+},
+
+    {timestamps: true},
+
+);
+
+const UserModel = mongoose.model("User", UserSchema);
+
+export default UserModel;
